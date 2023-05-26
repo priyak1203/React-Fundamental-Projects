67b39dea59653b5891de36db2f70d6997e9ad5d5
Priydarshini Karanth <kpriya1203@gmail.com>
2023-05-26 12:56:01 +0530

    menu project

---
+++ 05-menu/src/Categories.jsx
@@ -0,0 +1,20 @@
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category) => {
+        return (
+          <button
+            key={category}
+            type="button"
+            className="btn"
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
