e6c40d69479d042eb53b040fb6af68da867fd954
Priydarshini Karanth <kpriya1203@gmail.com>
2023-05-28 16:51:57 +0530

    slider project

---
+++ 07-slider/src/App.jsx
@@ -0,0 +1,13 @@
+import SlickCarousel from '../SlickCarousel';
+import Carousel from './Carousel';
+
+function App() {
+  return (
+    <main>
+      <Carousel />
+      {/* <SlickCarousel /> */}
+    </main>
+  );
+}
+
+export default App;
