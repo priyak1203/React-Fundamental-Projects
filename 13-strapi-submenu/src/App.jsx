8ce59ed6c63caa4f757edeaf5cf8b2778adbfcae
Priydarshini Karanth <kpriya1203@gmail.com>
2023-06-05 16:41:07 +0530

    stripe submenu project

---
+++ 13-strapi-submenu/src/App.jsx
@@ -0,0 +1,14 @@
+import { Hero, Navbar, Sidebar, Submenu } from './components';
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
+    </main>
+  );
+}
+
+export default App;
---
+++ 13-strapi-submenu/src/context.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from 'react';
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [pageId, setPageId] = useState(null);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{ isSidebarOpen, openSidebar, closeSidebar, pageId, setPageId }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, useGlobalContext };
